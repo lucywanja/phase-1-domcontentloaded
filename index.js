c74efc81c44f09b4d92b7e68713e04b20786148a
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-04-10 00:45:39 +0300

    Completed the lab with DOMContentLoad Event

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function () {
+//   target the  paragraph element with the id text
+
+  let paragraph = document.getElementById("text");
+
+//   replace the text contet
+
+  paragraph.textContent = "This is really cool!";
+
+  //console.log(paragraph);
+})
